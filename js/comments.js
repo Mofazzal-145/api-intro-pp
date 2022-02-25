3a252ef9858ca257c291f6d0ed8daafe0a6ec77e
Mofazzal-145 <md.mofazzalhossain145@gmail.com>
2022-02-25 21:27:33 +0600

    comment file

---
+++ js/comments.js
@@ -0,0 +1,22 @@
+
+function loadComments(){
+    fetch('https://jsonplaceholder.typicode.com/comments')
+    .then(res => res.json())
+    .then(data => displayComments(data))
+}
+
+function displayComments(comments){
+    const commentsContainer = document.getElementById('comments');
+    for(const comment of comments){
+        const div = document.createElement('div');
+        div.classList.add('comment');
+        div.innerHTML = `
+        <h3>${comment.name} </h3>
+        <p>${comment.body} </p>
+        `;
+        commentsContainer.appendChild(div);
+
+    }
+
+}
+
